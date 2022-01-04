a82ee18ecd088c2861f63cc30ea5b04d733199b2
Ashish Bajracharya <ashish.bajracharya@unitechmedia.com.np>
2022-01-04 11:58:45 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Grid } from "@material-ui/core"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Grid container direction="column">
+      <Grid item>this is where the header will be</Grid>
+      <Grid item container>
+        <Grid xs={12}></Grid>
+      </Grid>
+    </Grid>
+  )
 }
 
-export default App;
+export default App
